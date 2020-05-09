4b706e7f09c03a14c6b63a143807d6c055f32368
mmatana <milojevic.maja@gmail.com>
2020-05-09 12:19:20 -0700

    Fiks mistake

--- components/app.js
+++ components/app.js
@@ -1,7 +1,7 @@
 class App {
   constructor(gradeTable) {
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
-    this.handleGetGradesError + this.handleGetGradesError.bind(this);
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.gradeTable = gradeTable;
   }
   handleGetGradesError(error) {
